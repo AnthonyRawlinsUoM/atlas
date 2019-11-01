24d997b12b230353888337ede4220e52bd135b31
Anthony Rawlins <anthony.rawlins@unimelb.edu.au>
2019-11-01 14:30:47 +1100

    stash

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ValidatorService } from '../validator.service';
 import { MailerService } from '../mailer.service';
