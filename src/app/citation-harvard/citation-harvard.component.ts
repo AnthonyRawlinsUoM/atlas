19fbc9d2fb88ff9101dc33c509bf81c7e8d627b7
AnthonyRawlinsUoM <rawlinsfamily@icloud.com>
2020-02-05 18:47:51 +1100

    FAQs fixes

--- src/app/citation-harvard/citation-harvard.component.ts
+++ src/app/citation-harvard/citation-harvard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, AfterViewInit, Input } from '@angular/core';
 import { CitationService } from '../citation.service';
 import { HttpParameterCodec } from "@angular/common/http";
 
@@ -7,17 +7,32 @@ import { HttpParameterCodec } from "@angular/common/http";
     templateUrl: './citation-harvard.component.html',
     styleUrls: ['./citation-harvard.component.css']
 })
-export class CitationHarvardComponent implements OnInit {
+export class CitationHarvardComponent implements AfterViewInit {
 
     @Input('doi') doi: string;
     constructor(private citationService: CitationService) { }
-    content: string = '<i class="circle notch icon"></i>';
+    content?: string;
 
-    ngOnInit() {
-        this.citationService.getCitation(encodeURIComponent(this.doi))
-            .subscribe(data => {
-                this.content = data.toString()
-            });
+    ngAfterViewInit() {
+        this.citationService.getCitation(encodeURIComponent(this.doi)).subscribe(resp => {
+            // display its headers
+            const keys = resp.headers.keys();
+            let headers = keys.map(key => `${key}: ${resp.headers.get(key)}`);
+            let body = { ... resp.body };
+            console.log(body);
+
+        });
+
+        //     .subscribe((data) => {
+        //         console.log(data);
+        //         this.content = data.toString();
+        //     },
+        // (err) => {
+        //     console.warn(err);
+        // },
+        // () => {
+        //     console.log('done GETting');
+        // });
     }
 
 }
--- src/app/citation.service.ts
+++ src/app/citation.service.ts
@@ -1,27 +1,48 @@
 import { Injectable } from '@angular/core';
 import { from, of, Observable, BehaviorSubject, combineLatest, throwError, forkJoin } from 'rxjs';
-import {tap, filter, map, concatMap, delay, toArray, catchError, scan } from 'rxjs/operators';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { tap, retry, filter, map, concatMap, delay, toArray, catchError, scan } from 'rxjs/operators';
+import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
 export class CitationService {
 
-    configUrl = '/harvardfromdoi/branches/master';
+    configUrl = 'https://fromdoi-atppui9es2iu.runkit.sh';
 
     constructor(private http: HttpClient) { }
 
-    getCitation(doi) {
-        // console.log(`GET https://anthonyrawlinsuom.runkit.io${this.configUrl}/${doi}`);
-
-        const requestOptions: Object = {
-            responseType: 'text'
+    // public getCitation(doi) {
+    //     console.log(`GET https://anthonyrawlinsuom.runkit.io${this.configUrl}/harvard/${doi}`);
+    //
+    //     const requestOptions: Object = {
+    //         responseType: 'text'
+    //     }
+    //     return this.http.get(`https://anthonyrawlinsuom.runkit.io${this.configUrl}/harvard/${doi}`, requestOptions);
+    // }
+    private handleError(error: HttpErrorResponse) {
+        if (error.error instanceof ErrorEvent) {
+            // A client-side or network error occurred. Handle it accordingly.
+            console.error('An error occurred:', error.error.message);
+        } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong,
+            console.error(
+                `Backend returned code ${error.status}, ` +
+                `body was: ${error.error}`);
         }
+        // return an observable with a user-facing error message
+        return throwError(
+            'Something bad happened; please try again later.');
+    };
 
-        return this.http.get(`${this.configUrl}/${doi}`, requestOptions).pipe(tap(
-            data => console.log(data),
-            error => console.log(error)
-        ));
+    public getCitation(doi) {
+        let citation_string: string;
+
+        let url = `${this.configUrl}/${doi}/harvard`;
+
+        console.log(url);
+
+        return this.http.get(url, { observe: 'response' });
     }
 }
