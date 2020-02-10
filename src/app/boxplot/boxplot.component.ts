80d4e0a648022bd56e80e7412ebdbe9369126d24
AnthonyRawlinsUoM <anthony.rawlins@uinmelb.edu.au>
2020-02-10 13:53:28 +1100

    Chart.js

--- src/app/boxplot/boxplot.component.ts
+++ src/app/boxplot/boxplot.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnChanges, ElementRef, SimpleChanges, Input, Output, NgZone } from '@angular/core';
 import { Chart, ChartData } from 'chart.js';
 import 'chartjs-chart-box-and-violin-plot';
+import { schemes } from '../Viridis';
 
 @Component({
   selector: 'app-boxplot',
@@ -9,19 +10,68 @@ import 'chartjs-chart-box-and-violin-plot';
 })
 export class BoxplotComponent implements OnInit {
 
+  /* See: https://github.com/datavisyn/chartjs-chart-box-and-violin-plot */
+
   @Input() source: ChartData;
 
   private chart: Chart;
 
-  private readonly boxPlotData: ChartData = {
-    labels: ['A', 'B', 'C'],
+  colors = schemes[2];
+
+  example: ChartData = {
+    labels: ['House Loss'],
     datasets: [{
-      label: 'Dataset 1',
-      backgroundColor: 'steelblue',
+      label: 'PB 0',
+      backgroundColor: this.colors.colors[0],
       data: <any[]>[
-        Array.from({length: 100}).map(() => Math.random()),
-        Array.from({length: 100}).map(() => Math.random() * 0.6 + 0.2),
         Array.from({length: 100}).map(() => Math.random())
+      ]
+    },
+    {
+      label: 'PB 1',
+      backgroundColor: this.colors.colors[1],
+      data: <any[]>[
+        Array.from({length: 100}).map(() => Math.random() * 0.6 + 0.2)
+      ]
+    },
+    {
+      label: 'PB 2',
+      backgroundColor: this.colors.colors[2],
+      data: <any[]>[
+
+          Array.from({length: 100}).map(() => Math.random() * 0.6 + 0.2)
+      ]
+    },
+    {
+      label: 'PB 3',
+      backgroundColor: this.colors.colors[3],
+      data: <any[]>[
+
+          Array.from({length: 100}).map(() => Math.random() * 0.6 + 0.2)
+      ]
+    },
+    {
+      label: 'PB 5',
+      backgroundColor: this.colors.colors[4],
+      data: <any[]>[
+
+          Array.from({length: 100}).map(() => Math.random() * 0.6 + 0.2)
+      ]
+    },
+    {
+      label: 'PB 10',
+      backgroundColor: this.colors.colors[5],
+      data: <any[]>[
+
+          Array.from({length: 100}).map(() => Math.random() * 0.6 + 0.2)
+      ]
+    },
+    {
+      label: 'PB 15',
+      backgroundColor: this.colors.colors[6],
+      data: <any[]>[
+
+          Array.from({length: 100}).map(() => Math.random() * 0.6 + 0.2)
       ]
     }]
   };
@@ -47,7 +97,7 @@ export class BoxplotComponent implements OnInit {
         const node: HTMLElement = this.elementRef.nativeElement;
         this.chart = new Chart(node.querySelector('canvas'), {
           type: 'boxplot',
-          data: this.boxPlotData
+          data: this.example
         });
       });
     }
