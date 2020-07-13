051cc2327be3d2e8d7e15b1daa22bab90a4bc271
Anthony Rawlins <anthony.rawlins@unimelb.edu.au>
2020-07-14 04:50:32 +1000

    Video trial

--- src/app/help/topics/topics.component.ts
+++ src/app/help/topics/topics.component.ts
@@ -10,15 +10,15 @@ export class TopicsComponent implements OnInit {
   @ViewChild(PlayerComponent, {static: false}) player;
 
   topics:Array<IMedia> = [
-    {title: 'Introduction', runtime: '1:23', src: 'http://static.videogular.com/assets/videos/videogular.mp4', type: 'video/mp4'},
-    {title: 'Study Regions', runtime: '1:23', src: '', type: 'video/mp4'},
-    {title: 'Using the Map View', runtime: '1:23', src: '', type: 'video/mp4'},
-    {title: 'Overview of Comparators', runtime: '1:23', src: '', type: 'video/mp4'},
-    {title: 'Comparing Treatments', runtime: '1:23', src: '', type: 'video/mp4'},
-    {title: 'Understanding the Metrics', runtime: '1:23', src: '', type: 'video/mp4'},
-    {title: 'Interpretting the Results', runtime: '1:23', src: '', type: 'video/mp4'},
-    {title: 'The Effects of Climate Change', runtime: '1:23', src: '', type: 'video/mp4'},
-    {title: 'Comparing Costs', runtime: '1:23', src: '', type: 'video/mp4'}
+    {title: 'Introduction', runtime: '1:53', src: '/assets/videos/raw_source.mp4', type: 'video/mp4'},
+    {title: 'Study Regions', runtime: '1:53', src: '/assets/videos/raw_source.mp4', type: 'video/mp4'},
+    {title: 'Using the Map View', runtime: '1:53', src: '/assets/videos/raw_source.mp4', type: 'video/mp4'},
+    {title: 'Overview of Comparators', runtime: '1:53', src: '/assets/videos/raw_source.mp4', type: 'video/mp4'},
+    {title: 'Comparing Treatments', runtime: '1:53', src: '/assets/videos/raw_source.mp4', type: 'video/mp4'},
+    {title: 'Understanding the Metrics', runtime: '1:53', src: '/assets/videos/raw_source.mp4', type: 'video/mp4'},
+    {title: 'Interpretting the Results', runtime: '1:53', src: '/assets/videos/raw_source.mp4', type: 'video/mp4'},
+    {title: 'The Effects of Climate Change', runtime: '1:53', src: '/assets/videos/raw_source.mp4', type: 'video/mp4'},
+    {title: 'Comparing Costs', runtime: '1:53', src: '/assets/videos/raw_source.mp4', type: 'video/mp4'}
   ]
 
   constructor() { }
