0b7c281a825904979316042eb0e5d6fb1c13123e
Anthony Rawlins <anthony.rawlins@unimelb.edu.au>
2020-07-14 05:40:25 +1000

    fix video url

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -11,41 +11,41 @@ export class HelpComponent implements OnInit {
     helpsite:string = "https://documentation.prescribedburnatlas.science/";
 
     topics:Array<IMedia> = [
-      {title: 'Introduction', runtime: '1:53', src: 'https://prescribedburnatlas.science/assets/videos/raw_source.mp4', type: 'video/mp4', tags: [
+      {title: 'Introduction', runtime: '1:53', src: 'https://prescribedburnatlas.science/assets/video/raw_source.mp4', type: 'video/mp4', tags: [
         'Navigation',
         'Overview',
         'Browsing',
         'About',
         'Team'
       ]},
-      {title: 'Study Regions', runtime: '1:53', src: 'https://prescribedburnatlas.science/assets/videos/raw_source.mp4', type: 'video/mp4', tags: [
+      {title: 'Study Regions', runtime: '1:53', src: 'https://prescribedburnatlas.science/assets/video/raw_source.mp4', type: 'video/mp4', tags: [
         'Selecting',
         'Choices of landscape'
       ]},
-      {title: 'Using the Map View', runtime: '1:53', src: 'https://prescribedburnatlas.science/assets/videos/raw_source.mp4', type: 'video/mp4', tags: [
+      {title: 'Using the Map View', runtime: '1:53', src: 'https://prescribedburnatlas.science/assets/video/raw_source.mp4', type: 'video/mp4', tags: [
         'Panning',
         'Zooming',
         'Re-centering'
       ]},
-      {title: 'Overview of Comparators', runtime: '1:53', src: 'https://prescribedburnatlas.science/assets/videos/raw_source.mp4', type: 'video/mp4', tags: [
+      {title: 'Overview of Comparators', runtime: '1:53', src: 'https://prescribedburnatlas.science/assets/video/raw_source.mp4', type: 'video/mp4', tags: [
         'Costs',
         'Risks',
         'Plot types',
         'Interpretting results'
       ]},
-      {title: 'Comparing Treatments', runtime: '1:53', src: 'https://prescribedburnatlas.science/assets/videos/raw_source.mp4', type: 'video/mp4', tags: [
+      {title: 'Comparing Treatments', runtime: '1:53', src: 'https://prescribedburnatlas.science/assets/video/raw_source.mp4', type: 'video/mp4', tags: [
         'Edge',
         'Landscape',
         'Reasons for regimes'
       ]},
-      {title: 'Understanding the Metrics', runtime: '1:53', src: 'https://prescribedburnatlas.science/assets/videos/raw_source.mp4', type: 'video/mp4', tags: [
+      {title: 'Understanding the Metrics', runtime: '1:53', src: 'https://prescribedburnatlas.science/assets/video/raw_source.mp4', type: 'video/mp4', tags: [
         'How to read fire impact'
       ]},
-      {title: 'Interpretting the Results', runtime: '1:53', src: 'https://prescribedburnatlas.science/assets/videos/raw_source.mp4', type: 'video/mp4', tags: [
+      {title: 'Interpretting the Results', runtime: '1:53', src: 'https://prescribedburnatlas.science/assets/video/raw_source.mp4', type: 'video/mp4', tags: [
         'Interpretting results',
         'Comparing study areas'
       ]},
-      {title: 'The Effects of Climate Change', runtime: '1:53', src: 'https://prescribedburnatlas.science/assets/videos/raw_source.mp4', type: 'video/mp4', tags: [
+      {title: 'The Effects of Climate Change', runtime: '1:53', src: 'https://prescribedburnatlas.science/assets/video/raw_source.mp4', type: 'video/mp4', tags: [
         'How climate change affects the results',
         'Best case',
         'Worst case'
